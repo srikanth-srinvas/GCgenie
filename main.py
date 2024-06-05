ba7acc4a1eb0b4befd38e1f0fd6ac11bea5165b4
srikanth-srinivas <srikanth.2001@outlook.com>
2024-06-05 15:29:25 +0530

    updated main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from gc_content import parse_sequences, calculate_gc_content, gc_content_sliding
 from visualization import plot_gc_content, plot_gc_content_sliding_window
 
 def main():
-    parser = argparse.ArgumentParser(description="GC Content Calculation and Visualization Tool")
+    parser = argparse.ArgumentParser(description="GCgenie - A GC Content Calculation and Visualization Tool")
     parser.add_argument("-i", "--input", required=True, help="Path to the input FASTA or FASTQ file")
     parser.add_argument("-w", "--window", type=int, default=None, help="Size of the sliding window")
     parser.add_argument("-o", "--output", default="gc_content_plot.png", help="Output file name for the plot")
